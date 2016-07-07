aa601d0132704c8b4a95f5b5404c2e3c7ea53a5b
liudong <java_liudong@163.com>
2016-07-07 09:40:40 +0800

    删除文件

--- sh01.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# 描述：第一个hello world
-# 时间：2016.04.27 周三 21：39
-# 作者：刘泽栋
-
-PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
-export PATH
-
-echo "Hello World \r\n"
