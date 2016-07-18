3997d5ea241f48c81e9b77c6d835a2fa46a52790
liudong <java_liudong@163.com>
2016-07-18 11:42:09 +0800

    修改文件，这个文件 应该没用啦

--- bascbash2/cplist.sh
+++ bascbash2/cplist.sh
@@ -10,21 +10,21 @@
 forFileName=`cat ./line.txt`
 
 # 发布的项目名称
-#projectName=epartner   # 渠道
-projectName=B2C_Fabu-	# 电商
+projectName=epartner   # 渠道
+#projectName=B2C_Fabu-	# 电商
 
 
 # 这个是用来分割的目录
-#taget='com\.isoftstone\.iics\.bizsupport\.epartner/'   # 渠道
-taget='com.isoftstone.iics.www/'		# 电商
+taget='com\.isoftstone\.iics\.bizsupport\.epartner/'   # 渠道
+#taget='com.isoftstone.iics.www/'		# 电商
 
 # 项目的目录
-#dir=/home/liuzedong/workspace/.metadata/.plugins/org.eclipse.wst.server.core/tmp2/wtpwebapps/com.isoftstone.iics.bizsupport.epartner	# 渠道
-dir=/home/liuzedong/workspace/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/com.isoftstone.iics.www			# 电商
+dir=/home/liuzedong/workspace/.metadata/.plugins/org.eclipse.wst.server.core/tmp2/wtpwebapps/com.isoftstone.iics.bizsupport.epartner	# 渠道
+#dir=/home/liuzedong/workspace/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/com.isoftstone.iics.www			# 电商
 # 创建一个目录，来存放那些文件
 tagetDir=/tmp/project
 # 打包后jar的位置
-jardir=/media/liuzedong/0002239100048DF2/answern/发布的jar包/电商
+jardir=/media/liuzedong/0002239100048DF2/answern/发布的jar包/渠道
 
 # 创建这个目录，如果存在，就删除，然后创建。不存就创建
 if [ -d "${tagetDir}" ]; then
