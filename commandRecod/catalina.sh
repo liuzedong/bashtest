36762a2566cc4bb43cedbc96942ca12bd0fb5943
liudong <java_liudong@163.com>
2017-05-31 13:54:43 +0800

    添加, Tomcat start 启动脚本的描述

--- commandRecod/catalina.sh
+++ commandRecod/catalina.sh
@@ -203,19 +203,24 @@ fi
 # 因为上面的没走, 所以CLASSPATH= , 所以 CLASSPATH="CATALINA_HOME"/bin/bootstrap.jar = /home/liuzedong/tools/apache-tomcat-7.0.8/bin/bootstrap.jar
 CLASSPATH="$CLASSPATH""$CATALINA_HOME"/bin/bootstrap.jar
 
+#### 因为没有定义CATALINA_BASE 变量, 所以CATALINA_BASE=/home/liuzedong/tools/apache-tomcat-7.0.8 ####
 if [ -z "$CATALINA_BASE" ] ; then
   CATALINA_BASE="$CATALINA_HOME"
 fi
 
+#### 指定输出文件, 为空,所以,设置 CATALINA_OUT=/home/liuzedong/tools/apache-tomcat-7.0.8/logs/catalina.out ####
 if [ -z "$CATALINA_OUT" ] ; then
   CATALINA_OUT="$CATALINA_BASE"/logs/catalina.out
 fi
 
+#### 缓存目录,没有设置,所以定义 CATALINA_TMPDIR=/home/liuzedong/tools/apache-tomcat-7.0.8/temp ####
 if [ -z "$CATALINA_TMPDIR" ] ; then
   # Define the java.io.tmpdir to use for Catalina
   CATALINA_TMPDIR="$CATALINA_BASE"/temp
 fi
 
+
+#### 因为有这个jar包,且可读, 所以 添加一个环境变量的包 CLASSPATH=/home/liuzedong/tools/apache-tomcat-7.0.8/bin/bootstrap.jar:/home/liuzedong/tools/apache-tomcat-7.0.8/bin/tomcat-juli.jar ####
 # Add tomcat-juli.jar to classpath
 # tomcat-juli.jar can be over-ridden per instance
 if [ -r "$CATALINA_BASE/bin/tomcat-juli.jar" ] ; then
@@ -226,10 +231,12 @@ fi
 
 # Bugzilla 37848: When no TTY is available, don't output to console
 have_tty=0
+#### tty 是Linux中的屏幕代号,下面的判断的true, 所以hava_tty=1 ####
 if [ "`tty`" != "not a tty" ]; then
     have_tty=1
 fi
 
+#### 不是cygwin系统,所系下面的方法不会进入 ####
 # For Cygwin, switch paths to Windows format before running java
 if $cygwin; then
   JAVA_HOME=`cygpath --absolute --windows "$JAVA_HOME"`
@@ -240,7 +247,11 @@ if $cygwin; then
   CLASSPATH=`cygpath --path --windows "$CLASSPATH"`
   JAVA_ENDORSED_DIRS=`cygpath --path --windows "$JAVA_ENDORSED_DIRS"`
 fi
+#### 到此结束,上面是定义的变量,没有定义Linux系统下 ####
 
+
+#### 检测,日志配置文件是否存在,不存在,则 检测 "$CATALINA_BASE"/conf/logging.properties=/home/liuzedong/tools/apache-tomcat-7.0.8/conf/logging.properties 此配置文件是否可读 ####
+#### 因为可读,所以,就设置Java 参数 LOGGING_CONFIG=-Djava.util.logging.config.file=/home/liuzedong/tools/apache-tomcat-7.0.8/conf/logging.properties ####
 # Set juli LogManager config file if it is present and an override has not been issued
 if [ -z "$LOGGING_CONFIG" ]; then
   if [ -r "$CATALINA_BASE"/conf/logging.properties ]; then
@@ -251,6 +262,8 @@ if [ -z "$LOGGING_CONFIG" ]; then
   fi
 fi
 
+#### $LOGGING_MANAGER 为空 ####
+#### 检测,是否有日志的管理器,可以自己设置, 如果没有, 就设置apache的 JAVA_OPTS= -Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager ####
 if [ -z "$LOGGING_MANAGER" ]; then
   JAVA_OPTS="$JAVA_OPTS -Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager"
 else
@@ -259,6 +272,12 @@ fi
 
 # ----- Execute The Requested Command -----------------------------------------
 
+#### 检测,tty是否为1,因为上面有定义, 所以下面的方法会进入, 主要是打印出当前的目录
+#### Using CATALINA_BASE:   /home/liuzedong/tools/apache-tomcat-7.0.8
+#### Using CATALINA_HOME:   /home/liuzedong/tools/apache-tomcat-7.0.8
+#### Using CATALINA_TMPDIR: /home/liuzedong/tools/apache-tomcat-7.0.8/temp
+#### 因为$1=start,所以打印else 中的 Using JRE_HOME:        /usr/local/java/jdk1.7.0_79
+#### Using CLASSPATH:       /home/liuzedong/tools/apache-tomcat-7.0.8/bin/bootstrap.jar:/home/liuzedong/tools/apache-tomcat-7.0.8/bin/tomcat-juli.jar
 # Bugzilla 37848: only output this if we have a TTY
 if [ $have_tty -eq 1 ]; then
   echo "Using CATALINA_BASE:   $CATALINA_BASE"
@@ -275,6 +294,8 @@ if [ $have_tty -eq 1 ]; then
   fi
 fi
 
+
+#### 下面,是 start=jpda ####
 if [ "$1" = "jpda" ] ; then
   if [ -z "$JPDA_TRANSPORT" ]; then
     JPDA_TRANSPORT="dt_socket"
@@ -291,7 +312,10 @@ if [ "$1" = "jpda" ] ; then
   CATALINA_OPTS="$CATALINA_OPTS $JPDA_OPTS"
   shift
 fi
+#### 判断结束 ####
 
+
+#### 下面是,start = debug 判断 ####
 if [ "$1" = "debug" ] ; then
   if $os400; then
     echo "Debug command not available on OS400"
@@ -322,7 +346,14 @@ if [ "$1" = "debug" ] ; then
         org.apache.catalina.startup.Bootstrap "$@" start
     fi
   fi
+#### 到此结束,start = debug 配置 ####
 
+
+
+
+
+
+#### 下面是,start = run 判断 ####
 elif [ "$1" = "run" ]; then
 
   shift
@@ -347,9 +378,13 @@ elif [ "$1" = "run" ]; then
       -Djava.io.tmpdir=\"$CATALINA_TMPDIR\" \
       org.apache.catalina.startup.Bootstrap "$@" start
   fi
+#### 到此结束,start = run 配置 ####
 
+
+
+#### 下面是,start = start  因为,默认使用的,是start启动,所以下面的方法会走 ####
 elif [ "$1" = "start" ] ; then
-
+  #### 此处的CATALINA_PID 一般会空, 所以, ! -z $CATALINA_PID  为false
   if [ ! -z "$CATALINA_PID" ]; then
     if [ -f "$CATALINA_PID" ]; then
       if [ -s "$CATALINA_PID" ]; then
@@ -387,9 +422,17 @@ elif [ "$1" = "start" ] ; then
       fi
     fi
   fi
+  #### 到此处是判断CATALINA_PID 结束的位置 ####
 
+
+
+  #### 直接使用shift命令 , 就是将参数,向左移动, ####
   shift
+
+  #### touch /home/liuzedong/tools/apache-tomcat-7.0.8/logs/catalina.out, 创建日志输出文件 ####
   touch "$CATALINA_OUT"
+
+  #### 因为, 使用shift 将参数, 向左移动勒, 所以$1= 空, 取第二个参数, 所以,下面的判断也 不进去,进入else 方法 ####
   if [ "$1" = "-security" ] ; then
     if [ $have_tty -eq 1 ]; then
       echo "Using Security Manager"
@@ -404,7 +447,16 @@ elif [ "$1" = "start" ] ; then
       -Djava.io.tmpdir=\"$CATALINA_TMPDIR\" \
       org.apache.catalina.startup.Bootstrap "$@" start \
       >> "$CATALINA_OUT" 2>&1 "&"
-
+  #### 此处就是 主要的 启动方法 ####
+  #### /usr/local/java/jdk1.7.0_79/bin/java 
+  #### -Djava.util.logging.config.file=/home/liuzedong/tools/apache-tomcat-7.0.8/conf/logging.properties
+  #### -Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager
+  #### -Djava.endorsed.dirs="/home/liuzedong/tools/apache-tomcat-7.0.8/endorsed
+  #### -classpath /home/liuzedong/tools/apache-tomcat-7.0.8/bin/bootstrap.jar:/home/liuzedong/tools/apache-tomcat-7.0.8/bin/tomcat-juli.jar
+  #### -Dcatalina.base="/home/liuzedong/tools/apache-tomcat-7.0.8"
+  #### -Dcatalina.home="/home/liuzedong/tools/apache-tomcat-7.0.8"
+  #### -Djava.io.tmpdir="/home/liuzedong/tools/apache-tomcat-7.0.8/temp"
+  #### org.apache.catalina.startup.Bootstrap start &
   else
     eval \"$_RUNJAVA\" \"$LOGGING_CONFIG\" $JAVA_OPTS $CATALINA_OPTS \
       -Djava.endorsed.dirs=\"$JAVA_ENDORSED_DIRS\" -classpath \"$CLASSPATH\" \
@@ -415,11 +467,15 @@ elif [ "$1" = "start" ] ; then
       >> "$CATALINA_OUT" 2>&1 "&"
 
   fi
+  
 
+  #### CATALINA_PID 变量为空, 所以下面的判断是进不去的 ####
   if [ ! -z "$CATALINA_PID" ]; then
     echo $! > "$CATALINA_PID"
   fi
 
+
+#### 因为进入的 start方法, 所以下面的 stop 方法,没有进入 ####
 elif [ "$1" = "stop" ] ; then
 
   shift
